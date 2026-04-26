b5dd686552352700a6741c96d29ff1ed8f27b30b
grokljay-lang <grokljay@gmail.com>
2026-04-26 15:07:19 +0300

    update api url

--- src/api/forge.js
+++ src/api/forge.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://forge-backend-e6je.onrender.com' })
+const api = axios.create({ baseURL: 'https://forge-backend-production-84fc.up.railway.app' })
 
 export const generatePlan = (data) => api.post('/generate', data).then(r => r.data)
 export const generateTips = (data) => api.post('/tips', data).then(r => r.data)
